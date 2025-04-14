ee2ae94b4f540c6576af4b9b05cc2bc1cd07da99
AndrewOdiit <andrewodiit2020@gmail.com>
2025-04-14 20:08:12 +0300

    [orderbook] implement helper function `match_incoming_side` to keep code DRY

--- src/orderbook.rs
+++ src/orderbook.rs
@@ -1,6 +1,12 @@
-use std::collections::{BTreeMap, VecDeque};
+use std::{
+    collections::{BTreeMap, VecDeque},
+    time::SystemTime,
+};
 
-use crate::orders::{Order, Side};
+use crate::{
+    orders::{Order, Side},
+    trade::{self, Trade},
+};
 
 pub struct OrderBook {
     pub bids: BTreeMap<u64, VecDeque<Order>>,
@@ -21,6 +27,56 @@ impl<'a> Iterator for EitherIter<'a> {
             EitherIter::Rev(iter) => iter.next(),
         }
     }
+}
+//helper function
+fn match_incoming_side(
+    incoming: &mut Order,
+    book_side: &mut BTreeMap<u64, VecDeque<Order>>,
+    reversed: bool,
+) -> Vec<Trade> {
+    let mut trades = Vec::new();
+    let mut levels_to_remove = Vec::new();
+    //choose iterator
+    let iter = if reversed {
+        EitherIter::Rev(book_side.iter_mut().rev())
+    } else {
+        EitherIter::Fwd(book_side.iter_mut())
+    };
+    //Labeled loop so we can break out from inner while loop
+    'outer: for (&price, orders_at_price) in iter {
+        while let Some(order) = orders_at_price.front_mut() {
+            let trade_qty = incoming.quantity.min(order.quantity);
+            //Record the trade
+            trades.push(Trade {
+                price,
+                quantity: trade_qty,
+                maker_id: order.id,
+                taker_id: incoming.id,
+                timestamp: SystemTime::now(),
+            });
+            //Adjust quantities
+            incoming.quantity -= trade_qty;
+            order.quantity -= trade_qty;
+
+            //if the resting order is fully filled, remove it
+            if order.quantity == 0 {
+                orders_at_price.pop_front();
+            }
+            //if incoming order is fully filled , break out of both loops
+            if incoming.quantity == 0 {
+                break 'outer;
+            }
+        }
+        // if this price level is fully consumed, mark for removal
+        if orders_at_price.is_empty() {
+            levels_to_remove.push(price);
+        }
+    }
+    //Remove the emptied price levels
+    for price in levels_to_remove {
+        book_side.remove(&price);
+    }
+    trades
 }
 impl OrderBook {
     pub fn new() -> Self {
