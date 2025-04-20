ef24ca1e38567feafbc3055627e6a48c51020d1b
AndrewOdiit <andrewodiit2020@gmail.com>
2025-04-20 23:25:58 +0300

    [API] add appState.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use cli::run_cli;
 pub mod cli;
 pub mod orderbook;
 pub mod orders;
+pub mod state;
 pub mod trade;
 fn main() {
     run_cli();
---
+++ src/state.rs
@@ -0,0 +1,22 @@
+use crate::{orderbook::OrderBook, trade::Trade};
+use std::sync::{Arc, Mutex};
+
+pub struct AppState {
+    pub order_book: Arc<Mutex<OrderBook>>,
+    pub trade_log: Arc<Mutex<Vec<Trade>>>,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self {
+            order_book: Arc::new(Mutex::new(OrderBook::new())),
+            trade_log: Arc::new(Mutex::new(Vec::new())),
+        }
+    }
+}
+
+impl Default for AppState {
+    fn default() -> Self {
+        AppState::new()
+    }
+}
