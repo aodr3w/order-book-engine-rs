a342cdc64c9237b8743cf275ab74a25bff0d4130
AndrewOdiit <andrewodiit2020@gmail.com>
2025-04-27 17:47:35 +0300

    [state] add trade & book broadcast channels to AppState

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,5 @@
+use tokio::sync::broadcast;
+
 use crate::{orderbook::OrderBook, trade::Trade};
 use std::sync::{Arc, Mutex};
 
@@ -5,13 +7,19 @@ use std::sync::{Arc, Mutex};
 pub struct AppState {
     pub order_book: Arc<Mutex<OrderBook>>,
     pub trade_log: Arc<Mutex<Vec<Trade>>>,
+    pub trade_tx: broadcast::Sender<Trade>,
+    pub book_tx: broadcast::Sender<()>,
 }
 
 impl AppState {
     pub fn new() -> Self {
+        let (trade_tx, _) = broadcast::channel(1024); //size ??
+        let (book_tx, _) = broadcast::channel(16); //size ??
         Self {
             order_book: Arc::new(Mutex::new(OrderBook::new())),
             trade_log: Arc::new(Mutex::new(Vec::new())),
+            trade_tx,
+            book_tx,
         }
     }
 }
