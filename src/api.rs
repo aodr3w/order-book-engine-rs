f191443e9e15ee34d55bfa8923b45629471f0f8b
AndrewOdiit <andrewodiit2020@gmail.com>
2025-04-28 23:23:33 +0300

    [api] add web-socket end point

--- src/api.rs
+++ src/api.rs
@@ -1,10 +1,14 @@
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::time::SystemTime;
 use tracing::{info, warn};
 
 use axum::{
     Json, Router, debug_handler,
-    extract::{Path, State},
+    extract::{
+        Path, State, WebSocketUpgrade,
+        ws::{Message, WebSocket},
+    },
     http::StatusCode,
     response::IntoResponse,
     routing::{delete, get, post},
@@ -25,12 +29,18 @@ pub struct NewOrder {
     pub quantity: u64,
 }
 
-#[derive(serde::Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct BookSnapshot {
     pub bids: Vec<(u64, u64)>,
     pub asks: Vec<(u64, u64)>,
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(tag = "type", content = "data")]
+pub enum WsFrame {
+    Trade(Trade),
+    BookSnapshot(BookSnapshot),
+}
 #[derive(serde::Serialize)]
 pub struct OrderAck {
     order_id: u64,
@@ -98,15 +108,59 @@ pub async fn cancel_order(State(state): State<AppState>, Path(id): Path<u64>) ->
     }
 }
 
-// pub async  fn ws_handler(
-//     State(state): State<AppState>,
-//     ws: WebS
-// )
+pub async fn ws_handler(State(state): State<AppState>, ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(move |socket| handle_socket(socket, state))
+}
+
+pub async fn handle_socket(mut socket: WebSocket, state: AppState) {
+    let mut trade_rx = state.trade_tx.subscribe();
+    let mut book_rx = state.book_tx.subscribe();
+
+    //initial snapshot
+    let initial = {
+        let book = state.order_book.lock().unwrap();
+        BookSnapshot {
+            bids: book
+                .bids
+                .iter()
+                .rev()
+                .map(|(p, o)| (*p, o.iter().map(|o| o.quantity).sum()))
+                .collect(),
+            asks: book
+                .asks
+                .iter()
+                .map(|(p, o)| (*p, o.iter().map(|o| o.quantity).sum()))
+                .collect(),
+        }
+    };
+    let data = serde_json::to_string(&WsFrame::BookSnapshot(initial)).unwrap();
+    let _ = socket.send(Message::Text(data.into())).await;
+
+    loop {
+        tokio::select! {
+            Ok(trade) = trade_rx.recv() => {
+                if socket.send(Message::Text(serde_json::to_string(&WsFrame::Trade(trade)).unwrap().into())).await.is_err(){break;}
+            }
+            Ok(_) = book_rx.recv() => {
+                let snap = {
+                    let book = state.order_book.lock().unwrap();
+                    BookSnapshot {
+                        bids: book.bids.iter().rev().map(|(p,o)| (*p,o.iter().map(|o|o.quantity).sum())).collect(),
+                        asks: book.asks.iter().map(|(p,o)| (*p,o.iter().map(|o|o.quantity).sum())).collect()
+                    }
+                };
+                if socket.send(Message::Text(serde_json::to_string(&WsFrame::BookSnapshot(snap)).unwrap().into())).await.is_err(){break;}
+            } else => break
+        }
+    }
+}
+
 pub fn router(state: AppState) -> Router {
     Router::new()
         .route("/book", get(get_order_book))
         .route("/orders", post(create_order))
         .route("/trades", get(get_trade_log))
         .route("/orders/{id}", delete(cancel_order))
+        .route("/ws", get(ws_handler))
         .with_state(state)
 }
