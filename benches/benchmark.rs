b1d30c025c299848118c9e75bae883719d8b6d46
AndrewOdiit <andrewodiit2020@gmail.com>
2025-05-17 13:09:28 +0300

    benchmark orderbook

---
+++ benches/benchmark.rs
@@ -0,0 +1,66 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use order_book_engine::orderbook::OrderBook;
+use order_book_engine::orders::{Order, OrderType, Side};
+use std::time::SystemTime;
+
+fn setup_order_book(depth: u64, orders_per_level: u64) -> OrderBook {
+    let mut ob = OrderBook::new();
+    //populate asks
+    for price in 1..=depth {
+        for i in 0..orders_per_level {
+            // Sell side
+            ob.add_order(Order {
+                id: price * 1_000 + i,
+                side: Side::Sell,
+                order_type: OrderType::Limit,
+                price: Some(price),
+                quantity: 1,
+                timestamp: SystemTime::now(),
+            });
+            ob.add_order(Order {
+                id: (depth + price) * 1_000 + i,
+                side: Side::Buy,
+                order_type: OrderType::Limit,
+                price: Some(price),
+                quantity: 1,
+                timestamp: SystemTime::now(),
+            });
+        }
+    }
+    ob
+}
+
+fn bench_match_order(c: &mut Criterion) {
+    let depth = 100;
+    let orders_per_level = 10;
+    let mut ob = setup_order_book(depth, orders_per_level);
+    c.bench_function("match 1 market order", |b| {
+        b.iter(|| {
+            let market_buy = Order {
+                id: 0,
+                side: Side::Buy,
+                order_type: OrderType::Market,
+                price: None,
+                quantity: depth * orders_per_level / 2,
+                timestamp: SystemTime::now(),
+            };
+            ob.match_order(market_buy);
+        })
+    });
+
+    c.bench_function("match 1 limit crossing order", |b| {
+        b.iter(|| {
+            let limit_sell = Order {
+                id: 1,
+                side: Side::Sell,
+                order_type: OrderType::Limit,
+                price: Some(depth / 2),
+                quantity: depth * orders_per_level,
+                timestamp: SystemTime::now(),
+            };
+            ob.match_order(limit_sell)
+        })
+    });
+}
+criterion_group!(benches, bench_match_order);
+criterion_main!(benches);
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod orderbook;
+pub mod orders;
+pub mod trade;
