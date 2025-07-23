26c405bdf660211d3b5dec1260dcd4f488401270
AndrewOdiit <andrewodiit2020@gmail.com>
2025-07-23 23:06:48 +0300

    [main] seperate running server from simulation

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,31 @@
+use clap::{Parser, Subcommand};
+use order_book_engine::instrument::Pair;
 use order_book_engine::{api, instrument, market_maker, simulate, state::AppState};
 use serde_json::json;
 use std::path::Path;
+use tokio::task::JoinHandle;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
-use order_book_engine::instrument::Pair;
+#[derive(Parser)]
+#[command(name = "engine-cli")]
+#[command(
+    author = "Andrew Odiit",
+    version = "0.1",
+    about = "A demo of a limit-order-book-engine"
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    // Setup tracing
-    let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber)?;
+#[derive(Subcommand)]
+enum Commands {
+    Simulate { port: i32 },
+    Server { port: i32 },
+}
 
-    // The base URL our clients (Market Maker & Simulator) will use
-    let api_base = "http://127.0.0.1:3000".to_string();
-
-    // Launch our Axum server in the background
-    let state = AppState::new(Path::new("trade_store")).await.unwrap();
-    let app = api::router(state.clone());
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
-    let server_handle = tokio::spawn(async move {
-        tracing::info!("HTTP/WS server listening on http://0.0.0.0:3000");
-        // this will serve forever
-        axum::serve(listener, app).await.unwrap();
-    });
-
-    // small delay to let the server finish its bind
-    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-
+async fn run_simulation(api_base: String) -> anyhow::Result<Vec<JoinHandle<()>>> {
     // Seed the book with a resting bid @48 and ask @52
     let client = reqwest::Client::new();
     for (side, price) in &[("Buy", 48), ("Sell", 52)] {
@@ -68,7 +64,52 @@ async fn main() -> anyhow::Result<()> {
             tracing::error!("Simulation error: {:?}", e);
         }
     });
+    Ok(vec![sim_handle, mm_handler])
+}
 
-    let _ = tokio::join!(server_handle, sim_handle, mm_handler);
+async fn run_server(port: i32) -> anyhow::Result<JoinHandle<()>> {
+    // Launch our Axum server in the background
+    let state = AppState::new(Path::new("trade_store")).await.unwrap();
+    let app = api::router(state.clone());
+    let ep = format!("0.0.0.0:{port}");
+    let listener = tokio::net::TcpListener::bind(ep.clone()).await?;
+    let server_handle = tokio::spawn(async move {
+        tracing::info!("HTTP/WS server listening on {}", ep);
+        // this will serve forever
+        axum::serve(listener, app).await.unwrap();
+    });
+
+    // small delay to let the server finish its bind
+    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+    Ok(server_handle)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Setup tracing
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+    // The base URL our clients (Market Maker & Simulator) will use
+
+    let cli = Cli::parse();
+    let base = "http://127.0.0.1".to_string();
+    match cli.command {
+        //runs system with market_maker bot && client
+        Commands::Simulate { port } => {
+            let svh = run_server(port).await?;
+            let mut sh = run_simulation(format!("{}:{}", base, port)).await?;
+            sh.push(svh);
+            for j in sh {
+                j.await?;
+            }
+        }
+        Commands::Server { port } => {
+            let svh = run_server(port).await?;
+            svh.await?;
+        }
+    };
     Ok(())
 }
